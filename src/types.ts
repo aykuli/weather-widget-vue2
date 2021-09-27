49c749db430db6907129e2700703362bd3a7eed2
aynur <aykuli@ya.ru>
2021-09-27 12:04:21 +0700

    feat: init

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface WeatherData {
+  city?: string
+  temperature: string
+  feelsLike: string
+  humidity: number
+  windSpeed: number
+  pressure: number
+}
+
+export interface WidgetGeoLocation {
+  city?: string
+  stateCode?: string
+  countryCode?: string
+  latitude?: number
+  longitude?: number
+}
+
+export interface WidgetConfig {
+  city?: string
+  latitude?: number
+  longitude?: number
+}
