4ea36efe124c3de7adeff298a894c158ecbdaa12
aynur <aykuli@ya.ru>
2021-09-30 22:21:21 +0700

    Revert "feat: links"
    This reverts commit bb0d116ab2909e4a955ab376139d6a9a579975ab.

--- weather-widget.js
+++ weather-widget.js
@@ -13,10 +13,10 @@ class WeatherWidget extends HTMLDivElement {
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="icon" href="https://aykuli.github.io/weather-widget-vue2/favicon.ico" />
     <title>weather-widget2</title>
-    <link href="https://aykuli.github.io/weather-widget-vue2/app.e69be6cf.css" rel="preload" as="style" />
-    <link href="https://aykuli.github.io/weather-widget-vue2/app.33f5ffff.js" rel="preload" as="script" />
-    <link href="https://aykuli.github.io/weather-widget-vue2/chunk-vendors.3ef5530b.js" rel="preload" as="script" />
-    <link href="https://aykuli.github.io/weather-widget-vue2/app.e69be6cf.css" rel="stylesheet" />
+    <link href="https://aykuli.github.io/weather-widget-vue2/css/app.e69be6cf.css" rel="preload" as="style" />
+    <link href="https://aykuli.github.io/weather-widget-vue2/js/app.33f5ffff.js" rel="preload" as="script" />
+    <link href="https://aykuli.github.io/weather-widget-vue2/js/chunk-vendors.3ef5530b.js" rel="preload" as="script" />
+    <link href="https://aykuli.github.io/weather-widget-vue2/css/app.e69be6cf.css" rel="stylesheet" />
   </head>
   <body>
     <noscript
@@ -26,8 +26,8 @@ class WeatherWidget extends HTMLDivElement {
       ></noscript
     >
     <div id="app"></div>
-    <script src="https://aykuli.github.io/weather-widget-vue2/chunk-vendors.3ef5530b.js"></script>
-    <script src="https://aykuli.github.io/weather-widget-vue2/app.33f5ffff.js"></script>
+    <script src="https://aykuli.github.io/weather-widget-vue2/js/chunk-vendors.3ef5530b.js"></script>
+    <script src="https://aykuli.github.io/weather-widget-vue2/js/app.33f5ffff.js"></script>
   </body>
 </html>
 </iframe>
